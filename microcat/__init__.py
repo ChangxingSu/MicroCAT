40384d137ceb2fd576b3f29637701f31494050fa
ChangxingSu <505585725@qq.com>
2025-01-10 15:54:15 +0800

    fix: import parse_spatial_samples in __init__

--- microcat/__init__.py
+++ microcat/__init__.py
@@ -7,13 +7,12 @@ from microcat.configer import update_config
 from microcat.sample import HEADERS
 from microcat.sample import parse_samples
 from microcat.sample import parse_bam_samples
-
+from microcat.sample import parse_spatial_samples
 
 from microcat.sample import get_starsolo_sample_id
 from microcat.sample import get_sample_id
 from microcat.sample import get_fastqs_dir
 from microcat.sample import get_samples_bax
-
 from microcat.sample import get_samples_id_by_tissue
 from microcat.sample import get_samples_id_by_patient
 from microcat.sample import get_samples_id_by_lane
@@ -21,6 +20,7 @@ from microcat.sample import get_samples_id_by_library
 from microcat.sample import get_tissue_by_patient
 from microcat.sample import get_SAMattrRGline_by_sample
 from microcat.sample import get_SAMattrRGline_from_manifest
+from microcat.sample import get_img
 
 from microcat.__about__ import __version__, __author__
 
