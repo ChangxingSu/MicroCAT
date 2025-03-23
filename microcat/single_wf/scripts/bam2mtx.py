6b25b7a1e95777ac41c2f21fcda9c65f776c9ce2
ChangxingSu <505585725@qq.com>
2025-03-24 01:51:53 +0800

    feat(bam2mtx): implement Tree class for hierarchical taxonomic structures in bam2mtx
    - Update bam2mtx.py to utilize the Tree class for parsing Kraken taxonomy files, replacing the previous taxopy dependency.
    
    These changes improve the organization and functionality of taxonomic data management within the workflow.

--- microcat/single_wf/scripts/bam2mtx.py
+++ microcat/single_wf/scripts/bam2mtx.py
@@ -10,7 +10,6 @@ import pandas as pd
 import collections.abc
 import os
 import csv
-import taxopy
 # Create a logger object
 logger = logging.getLogger('my_logger')
 
@@ -36,6 +35,297 @@ logger.warning = lambda msg, *args, status=None: custom_log(logging.WARNING, msg
 logger.error = lambda msg, *args, status=None: custom_log(logging.ERROR, msg, *args, status=status)
 logger.debug = lambda msg, *args, status=None: custom_log(logging.DEBUG, msg, *args, status=status)
 
+
+# Tree Class
+# A class representing a node in a taxonomy tree used for constructing hierarchical taxonomic structures.
+# This class includes taxonomy levels and genomes identified in the Kraken report.
+class Tree(object):
+    'Tree node.'
+    
+    def __init__(self, taxid, name, level_rank, level_num, p_taxid, parent=None, children=None):
+        """
+        Initializes a Tree node with taxonomic information.
+
+        Parameters:
+        - taxid (int): Taxonomic identifier.
+        - name (str): Name of the taxonomic entity.
+        - level_rank (str): Taxonomic level rank (e.g., 'D' for Domain, 'P' for Phylum).
+        - level_num (int): Numeric representation of the taxonomic level.
+        - p_taxid (int): Parent taxonomic identifier.
+        - parent (Tree): Parent node in the tree.
+        - children (List[Tree]): List of child nodes.
+        """
+        self.taxid = taxid
+        self.name = name
+        self.level_rank = level_rank
+        self.level_num = int(level_num)
+        self.p_taxid = p_taxid
+        self.all_reads = 0
+        self.lvl_reads = 0
+
+        # Parent/children attributes
+        self.children = []
+        self.parent = parent
+        if children is not None:
+            for child in children:
+                self.add_child(child)
+
+    def add_child(self, node):
+        """
+        Adds a child node to the current node.
+
+        Parameters:
+        - node (Tree): Child node to be added.
+        """
+        assert isinstance(node, Tree)
+        self.children.append(node)
+
+    def taxid_to_desired_rank(self, desired_rank):
+        """
+        Retrieves the taxonomic identifier at the desired rank.
+
+        Parameters:
+        - desired_rank (str): Desired taxonomic rank.
+
+        Returns:
+        - int or str: Taxonomic identifier at the desired rank or an error message.
+        """
+        # Check if the current node's level_rank matches the desired_rank
+        if self.level_rank == desired_rank:
+            return self.taxid
+
+        child, parent, parent_taxid = self, None, None
+        while not parent_taxid == '1':
+            parent = child.parent
+            rank = parent.level_rank
+            parent_taxid = parent.taxid
+            if rank == desired_rank:
+                return parent.taxid
+            child = parent  # needed for recursion
+
+        # If no parent node is found or the desired_rank is not reached, return an error
+        return 'error - taxid above desired rank, or not annotated at desired rank'
+
+    def lineage_to_desired_rank(self, desired_parent_rank):
+        """
+        Retrieves the taxonomic lineage up to the desired parent rank.
+
+        Parameters:
+        - desired_parent_rank (str): Desired parent taxonomic rank.
+
+        Returns:
+        - List[int]: List of taxonomic identifiers in the lineage up to the desired parent rank.
+        """
+        lineage = []
+        lineage.append(self.taxid)
+
+        # Check if the current node's level_num is at the top level (1)
+        if self.level_num == "1":
+            return lineage
+
+        if self.level_rank in {"S", "G"}:
+            for child in self.children:
+                lineage.extend(child.get_all_descendants())
+
+        child, parent, parent_taxid = self, None, None
+        while not parent_taxid == '1':
+            parent = child.parent
+            rank = parent.level_rank
+            parent_taxid = parent.taxid
+            lineage.append(parent_taxid)
+            if rank == desired_parent_rank:
+                return lineage
+            child = parent  # needed for recursion
+        return lineage
+
+    def get_main_lvl_taxid(self):
+        """
+        Retrieves the taxonomic identifier at the main taxonomic level.
+
+        Returns:
+        - int: Taxonomic identifier at the main taxonomic level.
+        """
+        main_lvls = ['D', 'P', 'C', 'O', 'F', 'G', 'S']
+        level_rank = self.level_rank
+        child, parent, parent_taxid = self, None, None
+
+        while level_rank not in main_lvls:
+            parent = child.parent
+            level_rank = parent.level_rank
+            child = parent  # needed for recursion
+
+        main_lvl_taxid = child.taxid
+        return main_lvl_taxid
+    def get_all_descendants(self):
+        """
+        Get the taxids of all descendants in the subtree rooted at the current node.
+
+        Returns:
+        - list: List of taxids for all descendants in the subtree.
+        """
+        descendants_taxids = []
+
+        descendants_taxids.append(self.taxid)
+
+        for child in self.children:
+            descendants_taxids.extend(child.get_all_descendants())
+
+        return descendants_taxids
+    def get_mpa_path(self):
+        """
+        Retrieves the taxonomic path formatted for the Metagenomics Pathway Analysis (MPA) tool.
+
+        Returns:
+        - str: Formatted taxonomic path for MPA.
+        """
+        mpa_path = []
+        main_lvls = ['D', 'P', 'C', 'O', 'F', 'G', 'S']
+
+        # Create level name
+        level_rank = self.level_rank
+        name = self.name
+        name = name.replace(' ', '_')
+
+        if level_rank not in main_lvls:
+            level_rank = "x"
+        elif level_rank == "K":
+            level_rank = "k"
+        elif level_rank == "D":
+            level_rank = "d"
+
+        child, parent, parent_taxid = self, None, None
+        level_str = level_rank.lower() + "__" + name
+        mpa_path.append(level_str)
+
+        while not parent_taxid == '1':
+            parent = child.parent
+            level_rank = parent.level_rank
+            parent_taxid = parent.taxid
+            name = parent.name
+            name = name.replace(' ', '_')
+
+            try:
+                if level_rank not in main_lvls:
+                    level_rank = "x"
+                elif level_rank == "K":
+                    level_rank = "k"
+                elif level_rank == "D":
+                    level_rank = "d"
+
+                level_str = level_rank.lower() + "__" + name
+                mpa_path.append(level_str)
+            except ValueError:
+                raise
+
+            child = parent  # needed for recursion
+        # Reverse the MPA path list and join its components with "|".
+        mpa_path = "|".join(map(str, mpa_path[::-1])) 
+        return mpa_path
+
+    def is_microbiome(self):
+        """
+        Checks if the taxonomic node represents a microbiome entity.
+
+        Returns:
+        - bool: True if the node represents a microbiome, False otherwise.
+        """
+        is_microbiome = False
+        main_lvls = ['D', 'P', 'C', 'O', 'F', 'G', 'S']
+        lineage_name = []
+
+        # Create level name
+        level_rank = self.level_rank
+        name = self.name
+        name = name.replace(' ', '_')
+        lineage_name.append(name)
+
+        if level_rank not in main_lvls:
+            level_rank = "x"
+        elif level_rank == "K":
+            level_rank = "k"
+        elif level_rank == "D":
+            level_rank = "d"
+
+        child, parent, parent_taxid = self, None, None
+
+        while not parent_taxid == '1':
+            parent = child.parent
+            level_rank = parent.level_rank
+            parent_taxid = parent.taxid
+            name = parent.name
+            name = name.replace(' ', '_')
+            lineage_name.append(name)
+            child = parent  # needed for recursion
+
+        if 'Fungi' in lineage_name or 'Bacteria' in lineage_name or 'Viruses' in lineage_name:
+            is_microbiome = True
+        return is_microbiome
+
+    def get_taxon_path(self):
+        """
+        Retrieves the taxonomic path including taxonomic identifiers and names.
+
+        Returns:
+        - List[str]: List containing taxonomic path as taxonomic identifiers and names.
+        """
+        kept_levels = ['D', 'P', 'C', 'O', 'F', 'G', 'S']
+        lineage_taxid = []
+        lineage_name = []
+        name = self.name
+        rank = self.level_rank
+        name = name.replace(' ', '_')
+        lineage_taxid.append(self.taxid)
+        lineage_name.append(name)
+
+        child, parent = self, None
+        while not rank == 'D':
+            parent = child.parent
+            rank = parent.level_rank
+            parent_taxid = parent.taxid
+            name = parent.name
+            name = name.replace(' ', '_')
+            if rank in kept_levels:
+                lineage_taxid.append(parent_taxid)
+                lineage_name.append(name)
+            child = parent  # needed for recursion
+
+        taxid_path = "|".join(map(str, lineage_taxid[::-1]))
+        taxsn_path = "|".join(map(str, lineage_name[::-1]))
+        return [taxid_path, taxsn_path]
+
+def make_dicts(ktaxonomy_file):
+    """
+    Parse a Kraken taxonomy file and create a dictionary of Tree nodes.
+
+    Parameters:
+    - ktaxonomy_file (str): Path to the Kraken taxonomy file.
+
+    Returns:
+    - dict: Dictionary mapping taxonomic identifiers to Tree nodes.
+    """
+    root_node = -1  # Initialize the root node identifier.
+    taxid2node = {}  # Dictionary to store Tree nodes mapped to their taxonomic identifiers.
+
+    with open(ktaxonomy_file, 'r') as kfile:
+        for line in kfile:
+            # Parse the tab-separated values from each line of the Kraken taxonomy file.
+            [taxid, p_tid, rank, lvl_num, name] = line.strip().split('\t|\t')
+            
+            # Create a Tree node for the current taxonomic entry.
+            curr_node = Tree(taxid, name, rank, lvl_num, p_tid)
+            
+            # Add the current node to the taxid2node dictionary.
+            taxid2node[taxid] = curr_node
+            
+            # Set parent and children relationships for the current node.
+            if taxid == "1":
+                root_node = curr_node
+            else:
+                curr_node.parent = taxid2node[p_tid]
+                taxid2node[p_tid].add_child(curr_node)
+
+    return taxid2node
+    
 def most_frequent(List):
     """Finds the most frequent element in a list"""
     return max(set(List), key = List.count)
@@ -48,7 +338,7 @@ def map_nested_dicts(ob, func):
         else:
             ob[k] = func(v)
 
-def twist_dict_UMI(nested,taxdb):
+def twist_dict_UMI(nested, taxid2node):
     """ Make count dictionary with {cellbarcode : {taxonomyID : transcriptcount}} """
     newdict = {}
     # Process cell-transcript relationships
@@ -65,29 +355,37 @@ def twist_dict_UMI(nested,taxdb):
 
     # Process genus-species relationships
     for ckey, tdict in newdict.items():
-        for kvalue, count in tdict.items():
-            # Check if kvalue has a genus in the same cellbarcode
-            taxon = taxopy.Taxon(int(kvalue), taxdb)
-            lineage = taxon.taxid_lineage            
-            # Add count to parent if it exists
-            for parent in lineage:
-                if parent in newdict[ckey] and parent != kvalue:
-                    newdict[ckey][parent] += count
+        for kvalue, count in list(tdict.items()):
+            # Get the lineage using taxid2node
+            if kvalue in taxid2node:
+                lineage = taxid2node[kvalue].get_taxon_path()[0].split('|')
+                # Add count to parent if it exists
+                for parent in lineage:
+                    if parent != kvalue and parent in taxid2node:
+                        if parent in newdict[ckey]:
+                            newdict[ckey][parent] += count
+                        else:
+                            newdict[ckey][parent] = count
             
     return(newdict)
-def twist_dict(nested,taxdb):
+
+def twist_dict(nested, taxid2node):
     """ Make count dictionary with {cellbarcode : {taxonomyID : transcriptcount}} """
-    newdict = nested
+    newdict = nested.copy()  # Create a copy to avoid modifying during iteration
+    
     # Process genus-species relationships
     for ckey, tdict in newdict.items():
-        for kvalue, count in tdict.items():
-            # Check if kvalue has a genus in the same cellbarcode
-            taxon = taxopy.Taxon(int(kvalue), taxdb)
-            lineage = taxon.taxid_lineage            
-            # Add count to parent if it exists
-            for parent in lineage:
-                if parent in newdict[ckey] and parent != kvalue:
-                    newdict[ckey][parent] += count
+        for kvalue, count in list(tdict.items()):
+            # Get the lineage using taxid2node
+            if kvalue in taxid2node:
+                lineage = taxid2node[kvalue].get_taxon_path()[0].split('|')
+                # Add count to parent if it exists
+                for parent in lineage:
+                    if parent != kvalue and parent in taxid2node:
+                        if parent in newdict[ckey]:
+                            newdict[ckey][parent] += count
+                        else:
+                            newdict[ckey][parent] = count
             
     return(newdict)
 
@@ -126,8 +424,7 @@ def main():
     parser = argparse.ArgumentParser(description='This script is used to output bam classified microbial data in cellranger format as feature.tsv,barcodes.tsv,matrix.mtx \n This requires the additional packages pysam(If your python version is up to 3.9)\n')
     parser.add_argument('--cb_bam', help='Input align SAM or BAM file with CB', required=True)
     parser.add_argument('--align_result', help='Input align tsv containing read name, taxonomy id', required=True)
-    parser.add_argument('--nodes', help='Path for nodes.dmp file', required=True)
-    parser.add_argument('--names', help='Path for names.dmp file', required=True)
+    parser.add_argument('--ktaxonomy_file', help='Input kraken2 ktaxonomy file', required=True)
     parser.add_argument('--verbose', action='store_true', help='Detailed print')
     parser.add_argument('--profile_tsv', help='Output microbiome read tsv', required=True)
     parser.add_argument('--matrixfile', help='Output microbiome matrix', required=True)
@@ -148,12 +445,12 @@ def main():
     file_handler.setFormatter(log_format)
     logger.addHandler(file_handler)
 
-   # Loading taxonomy database
-    logger.info('Loading taxonomy database', status='run')
+    # Loading taxonomy database
+    logger.info('Loading taxonomy database from Kraken taxonomy file', status='run')
     try:
-        taxdb = taxopy.TaxDb(nodes_dmp=args.nodes,names_dmp=args.names)
-        logger.info('Successfully loading taxonomy database', status='complete')
-    except (ValueError, KeyError) as e:
+        taxid2node = make_dicts(args.ktaxonomy_file)
+        logger.info('Successfully loaded taxonomy database', status='complete')
+    except Exception as e:
         logger.error(f"An error occurred while loading taxonomy database: {e}")
         sys.exit()
 
@@ -185,7 +482,7 @@ def main():
     is_ub = False
     is_rg = False
     read_count = 0
-    with pysam.AlignmentFile(args.cb_bam, "rb") as barcode_file:
+    with pysam.AlignmentFile(args.cb_bam, "rb", check_sq = False) as barcode_file:
 
         for bread in barcode_file:
             # 检查是否存在目标标签
@@ -220,7 +517,7 @@ def main():
     if mode == "CB_UMI":
         # Store extracted information in nested dictionary {cellbarcode:{transcriptbarcode: taxonomyID}}
         nested_dict = {}
-        with pysam.AlignmentFile(args.cb_bam, "rb") as cb_file:
+        with pysam.AlignmentFile(args.cb_bam, "rb", check_sq = False) as cb_file:
 
             for bread in cb_file:
                 total_count += 1
@@ -257,11 +554,11 @@ def main():
         # Find most frequent taxonomy for each transcript
         map_nested_dicts(nested_dict, most_frequent)
         # Make sparse matrix
-        rows, cols, vals, cell_list, taxid_list = dict2lists(twist_dict_UMI(nested_dict,taxdb))
+        rows, cols, vals, cell_list, taxid_list = dict2lists(twist_dict_UMI(nested_dict, taxid2node))
     if mode == "CB":
         # Store extracted information in nested dictionary {cellbarcode:{transcriptbarcode: taxonomyID}}
         nested_dict = {}
-        with pysam.AlignmentFile(args.cb_bam, "rb") as cb_file:
+        with pysam.AlignmentFile(args.cb_bam, "rb",check_sq = False) as cb_file:
 
             for bread in cb_file:
                 total_count += 1
@@ -295,11 +592,11 @@ def main():
                 use_count += 1
                 
         # Make sparse matrix
-        rows, cols, vals, cell_list, taxid_list = dict2lists(twist_dict(nested_dict,taxdb))        
+        rows, cols, vals, cell_list, taxid_list = dict2lists(twist_dict(nested_dict, taxid2node))        
     if mode == "RG":
         # Store extracted information in nested dictionary {cellbarcode:{transcriptbarcode: taxonomyID}}
         nested_dict = {}
-        with pysam.AlignmentFile(args.cb_bam, "rb") as cb_file:
+        with pysam.AlignmentFile(args.cb_bam, "rb",check_sq = False) as cb_file:
 
             for bread in cb_file:
                 total_count += 1
@@ -333,7 +630,7 @@ def main():
                 use_count += 1
 
         # Make sparse matrix
-        rows, cols, vals, cell_list, taxid_list = dict2lists(twist_dict(nested_dict,taxdb))
+        rows, cols, vals, cell_list, taxid_list = dict2lists(twist_dict(nested_dict, taxid2node))
     logger.info(f'Parsing bam file complete, total reads: {total_count}, use reads: {use_count}, skipped reads: {skipped}', status='complete')
 
 
